96aabfc04fd2ba6c1f9ec8634c1932082760fd8d
plutoo <plutoo@univor.se>
2014-08-15 22:29:13 +0200

    Build fix.

--- ro_command_handler/source/svc.s
+++ ro_command_handler/source/svc.s
@@ -23,3 +23,24 @@ svc_mapProcessMemory:
 svc_unmapProcessMemory:
 	svc 0x72
 	bx lr
+
+.global svc_replyAndReceive
+.type svc_replyAndReceive, %function
+svc_replyAndReceive:
+              STR   R0, [SP,#-4]!
+              SVC   0x4F
+              LDR   R2, [SP]
+              STR   R1, [R2]
+              ADD   SP, SP, #4
+              BX    LR
+
+
+.global svc_acceptSession
+.type svc_acceptSession, %function
+svc_acceptSession:
+              STR   R0, [SP,#-4]!
+              SVC   0x4A
+              LDR   R2, [SP]
+              STR   R1, [R2]
+              ADD   SP, SP, #4
+              BX    LR
