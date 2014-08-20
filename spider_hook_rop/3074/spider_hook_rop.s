371464319186fb5c897b8de97d27d1bf9a66a426
smea <smealum@gmail.com>
2014-08-20 08:52:59 -0700

    ported spider_hook_rop to 3074 (this and previous commit untested CAUSE IM ON A TRAIIIIIN)

--- spider_hook_rop/3074/spider_hook_rop.s
+++ spider_hook_rop/3074/spider_hook_rop.s
@@ -5,8 +5,8 @@
 .create "spider_hook_rop.bin",0x0
 
 .orga 0x0
-	.word 0x0013035c ; pop {lr, pc}
+	.word 0x001303a4 ; pop {lr, pc}
 		.word SPIDER_INITIALROP_ADR ; lr => sp
-	.word 0x00130358 ; mov sp, lr | pop {lr, pc}
+	.word 0x001303a0 ; mov sp, lr | pop {lr, pc}
 
 .Close
